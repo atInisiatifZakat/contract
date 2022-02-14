f28444c3cb93980dc40e2c2e7122de5cb07d9d6b
kaizen-ci <info@kaizen-ci.org>
2022-02-14 04:04:51 +0000

    Release donation api (#187)
    * [Donation] Add `v3` prefix auth
    
    * [Donation] Remove validation
    
    * [Donation] Add `v3` ping route
    
    * [Donation] Remove status validation in create confirmation
    
    * [Donation] Add confirmation resources
    
    * [Donation] Fixing error params
    
    * [Donation] Fixing array error
    
    * [Donation] Change response
    
    * [Donation] Add date request body when paid
    
    * Change sanctum middleware
    
    * [Donation] Add include query builder
    
    * [Package][Common] Add `telescope`
    
    * [Project][Donation] Add telescope for local
    
    * [Project][Donation] Refactoring
    
    * [Donation] Refactoring donor detail
    
    * Apply ecs
    
    * [Donation] Add change employee
    
    * [Donation] Add `v3` prefix auth
    
    * [Donation] Remove validation
    
    * [Donation] Add `v3` ping route
    
    * [Donation] Remove status validation in create confirmation
    
    * [Donation] Add confirmation resources
    
    * [Donation] Fixing error params
    
    * [Donation] Fixing array error
    
    * [Donation] Change response
    
    * [Donation] Add date request body when paid
    
    * Change sanctum middleware
    
    * [Donation] Add include query builder
    
    * [Package][Common] Add `telescope`
    
    * [Project][Donation] Add telescope for local
    
    * [Project][Donation] Refactoring
    
    * [Donation] Refactoring donor detail
    
    * Apply ecs
    
    * [Donation] Add change employee
    
    * [Project][Donation] Add event
    
    * Add telecope
    
    * Add routing exclude
    
    * Add example
    
    * [Project][HRM] Add include
    
    * [Project][HRM] Add docker compose
    
    * [Project][Common] Update example
    
    * [Project][HRM] Some refactoring
    
    * [Project][Donation] Add donation user
    
    * [Project][Donation] Refactoring
    
    * [Project][Donation] Add testing
    
    * Apply ecs and psalm
    
    * Add default config
    
    * [Project][Donation] Add change partner
    
    * [Project][Donation] Update event
    
    * [Partner][Donation] Add change test
    
    * [Project][Donation] Add change partner
    
    * [Project][Donation] Add change outlet donor
    
    * [Project][Donation] Add order by
    
    * [Project][Donation] Add delete phone donor
    
    * [Donor] Add inactive donor
    
    * Fixing psalm type
    
    * Add doc type
    
    * [Project][Donation] Update donor show
    
    * [Project][Donation] Update donor show
    
    * [Donation] Change `npwp` to `TaxNumber`
    
    * [Donation] Add change donor type
    
    * [Donation] Add address (country and regency) filter
    
    * [Donation] Add tax number filter
    
    * [Donation] Add missing repository change type
    
    * [Shared] Add package `model-shared`
    
    * [Shared] Running merge command
    
    * [Project][Common] Install to common service
    
    * [Package][Common] Add `morphMap`
    
    * Add common service
    
    * [Package][Donation] Add `model-shared`
    
    * Update .env.example
    
    * Refactoring
    
    * Refactoring
    
    * Add donor jobs
    
    * add inactive show
    
    * [ModelShared] Add testing
    
    * [Donation] Add store donor job and testing
    
    * [ModelShared] Apply ecs and psalm
    
    * [Donation] Apply ecs and psalm
    
    * Add env
    
    * Update docker-compose.yml
    
    * Update workflow
    
    * [Donation] Fixing testing
    
    * [Donation] Fixing version
    
    * Update workflow
    
    * [ModelShared] Add testing required
    
    * Update workflow
    
    * update timeline project action
    
    * [Donation] Fixing create confirmation
    
    * [Donation] Change to string
    
    * [Donation] Add testing
    
    * [User][Account] Allow email username
    
    * [Donation] Refactoring create donor
    
    * [Donation] Add create and update tax
    
    * Update 2022_01_25_041217_change_source_id_to_string.php
    
    Co-authored-by: Annisa Nadia N <annisanadian@gmail.com>

--- src/Common/Repository/RequestFilterAwareInterface.php
+++ src/Common/Repository/RequestFilterAwareInterface.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Inisiatif\Package\Contract\Common\Repository;
 
+use Illuminate\Http\Request;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface RequestFilterAwareInterface
 {
+    /**
+     * @param Request $request
+     */
     public function filter($request): LengthAwarePaginator;
 }
