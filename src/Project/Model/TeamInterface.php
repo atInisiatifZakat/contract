41541910f56664a7721090a12129e93f7e55b69b
kaizen-ci <info@kaizen-ci.org>
2022-04-04 04:03:24 +0000

    Merge pull request #235 from atInisiatifZakat/develop
    Fixing testing

--- src/Project/Model/TeamInterface.php
+++ src/Project/Model/TeamInterface.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Inisiatif\Package\Contract\Project\Model;
 
+use Inisiatif\Package\User\Models\Employee;
 use Inisiatif\Package\Contract\Common\Model\ResourceInterface;
-use Inisiatif\Application\EasyDonation\Foundation\Entities\Employee;
 
 interface TeamInterface extends ResourceInterface, ProjectAwareInterface
 {
